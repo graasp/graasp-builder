0cbaa1e8ad415cfb1465236599d3f3954803e039
pyphilia <pyphilia@gmail.com>
2020-12-17 11:24:45 +0100

    refactor: move i18n provider in Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import configureStore from '../store/configure';
+import i18nConfig from '../config/i18n';
 
 const theme = createMuiTheme({
   palette: {
@@ -15,11 +17,13 @@ const theme = createMuiTheme({
 const { store } = configureStore();
 
 const Root = () => (
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <App />
-    </MuiThemeProvider>
-  </Provider>
+  <I18nextProvider i18n={i18nConfig}>
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </Provider>
+  </I18nextProvider>
 );
 
 export default Root;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
 import './index.css';
 import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
-import i18nConfig from './config/i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18nConfig}>
-      <Root />
-    </I18nextProvider>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 );
