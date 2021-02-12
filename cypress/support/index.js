f12a0ceaaaac331c5677e856bb47941a40032e49
pyphilia <pyphilia@gmail.com>
2021-02-12 18:30:35 +0100

    build: set up cypress in github actions

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,6 +12,8 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@cypress/code-coverage/support';
 
 // Import commands.js using ES2015 syntax:
 import './commands';
