3a043e9c6cf9fbd43ddccf1425a1d8994996c6a5
pyphilia <pyphilia@gmail.com>
2021-05-14 15:28:39 +0200

    refactor: apply PR requested changes

--- src/components/item/FileDocument.js
+++ src/components/item/FileDocument.js
@@ -3,7 +3,7 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const FileDocument = () => {
-  const [value, setValue] = useState('hello');
+  const [value, setValue] = useState(null);
 
   return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 };
--- src/utils/membership.js
+++ src/utils/membership.js
@@ -9,7 +9,7 @@ export const isSettingsEditionAllowedForUser = ({ memberships, memberId }) =>
       mId === memberId && PERMISSION_LEVELS.ADMIN === permission,
   );
 
-export const isItemEditionAllowedForUser = ({ memberships, memberId }) =>
+export const isItemUpdateAllowedForUser = ({ memberships, memberId }) =>
   memberships?.find(
     ({ memberId: mId, permission }) =>
       mId === memberId && PERMISSIONS_EDITION_ALLOWED.includes(permission),
