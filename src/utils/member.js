a9f15630e472f09a0bf4892dc9efe11f9c967145
Abdallah <abdallah.alchami1@gmail.com>
2021-06-21 15:44:22 +0200

    refactor: minor formatting fix

--- src/utils/member.js
+++ src/utils/member.js
@@ -1,6 +1,5 @@
 import { validate } from 'uuid';
 
-// eslint-disable-next-line import/prefer-default-export
 export const isMemberIdValid = (memberId) => validate(memberId?.trim());
 
 export const getMemberById = (members, id) =>
