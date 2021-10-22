acb94793d7cedea28f5bcb585ed44797d7a5f28f
pyphilia <pyphilia@gmail.com>
2021-10-22 13:39:02 +0200

    refactor: fix language switch depending on user setting

--- src/components/context/CurrentUserContext.js
+++ src/components/context/CurrentUserContext.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { hooks } from '../../config/queryClient';
+import i18n from '../../config/i18n';
 
 const CurrentUserContext = React.createContext();
 
 const { useCurrentMember } = hooks;
 const CurrentUserContextProvider = ({ children }) => {
-  const hook = useCurrentMember();
+  const query = useCurrentMember();
+
+  // update language depending on user setting
+  const lang = query?.data?.get('extra')?.lang;
+  useEffect(() => {
+    if (lang !== i18n.language) {
+      i18n.changeLanguage(lang);
+    }
+  }, [lang]);
 
   return (
-    <CurrentUserContext.Provider value={hook}>
+    <CurrentUserContext.Provider value={query}>
       {children}
     </CurrentUserContext.Provider>
   );
