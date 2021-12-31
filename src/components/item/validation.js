012719da2cac6ea6d52708e6aa5f22421ed6195e
Louise Wang <louise.w1997@gmail.com>
2021-12-31 15:59:06 +0100

    fix: fix bug in validation

--- src/components/item/validation.js
+++ src/components/item/validation.js
@@ -1,11 +1,15 @@
 import BadWordsFilter from 'bad-words';
+import { stripHtml } from '../../utils/item';
 
 const checkBadWrods = (documents) => {
+  const strings = documents?.filter(Boolean).map((entry) => stripHtml(entry));
   const badWordsFilter = new BadWordsFilter();
-  for (const entry in documents) {
-    if (badWordsFilter.isProfane(entry)) return false;
+  for (const index in strings) {
+    if (badWordsFilter.isProfane(strings[index])) {
+      return true;
+    }
   }
-  return true;
+  return false;
 };
 
 export default checkBadWrods;
