b2d69b369638ed5a398bb5b6760c17b777bb8954
pyphilia <pyphilia@gmail.com>
2022-02-10 14:29:54 +0100

    refactor: update app item data

--- src/utils/membership.js
+++ src/utils/membership.js
@@ -4,18 +4,36 @@ import { PERMISSION_LEVELS } from '../enums';
 // todo: better check with typescript
 export const isError = (memberships) => memberships?.statusCode;
 
-export const isSettingsEditionAllowedForUser = ({ memberships, memberId }) =>
-  memberships?.find(
-    ({ memberId: mId, permission }) =>
-      mId === memberId && PERMISSION_LEVELS.ADMIN === permission,
-  );
-
 export const isItemUpdateAllowedForUser = ({ memberships, memberId }) =>
   Boolean(
     memberships?.find(
       ({ memberId: mId, permission }) =>
         mId === memberId && PERMISSIONS_EDITION_ALLOWED.includes(permission),
     ),
+  );
+
+export const getBestPermissionForMemberFromMemberships = ({
+  memberships,
+  memberId,
+}) => {
+  const itemMemberships = memberships?.filter(
+    ({ memberId: mId }) => mId === memberId,
+  );
+  if (!itemMemberships) {
+    return null;
+  }
+
+  const sorted = itemMemberships?.sort(
+    (a, b) => a.itemPath.length > b.itemPath.length,
+  );
+
+  return sorted[0];
+};
+
+export const isSettingsEditionAllowedForUser = ({ memberships, memberId }) =>
+  memberships?.find(
+    ({ memberId: mId, permission }) =>
+      mId === memberId && PERMISSION_LEVELS.ADMIN === permission,
   );
 
 export const membershipsWithoutUser = (memberships, userId) =>
