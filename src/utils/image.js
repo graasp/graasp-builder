0125127201a85734653e1ad0a6a009fd5cb35fcd
pyphilia <pyphilia@gmail.com>
2022-04-08 15:49:21 +0200

    refactor: remove pixelratio from canvas cropping

--- src/utils/image.js
+++ src/utils/image.js
@@ -3,43 +3,35 @@ import { THUMBNAIL_EXTENSION } from '../config/constants';
 // eslint-disable-next-line import/prefer-default-export
 export const getCroppedImg = (image, crop, extension = THUMBNAIL_EXTENSION) => {
   const canvas = document.createElement('canvas');
-  const pixelRatio = window.devicePixelRatio;
   const scaleX = image.naturalWidth / image.width;
   const scaleY = image.naturalHeight / image.height;
   const ctx = canvas.getContext('2d');
 
-  canvas.width = crop.width * pixelRatio * scaleX;
-  canvas.height = crop.height * pixelRatio * scaleY;
-
-  ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-  ctx.imageSmoothingQuality = 'high';
+  canvas.width = crop.width * scaleX;
+  canvas.height = crop.height * scaleY;
 
   ctx.drawImage(
     image,
     crop.x * scaleX,
     crop.y * scaleY,
-    crop.width * scaleX,
-    crop.height * scaleY,
+    canvas.width,
+    canvas.height,
     0,
     0,
-    crop.width * scaleX,
-    crop.height * scaleY,
+    canvas.width,
+    canvas.height,
   );
 
   return new Promise((resolve, reject) => {
-    canvas.toBlob(
-      (blob) => {
-        if (!blob) {
-          reject(new Error('Canvas is empty'));
-          console.error('Canvas is empty');
-          return;
-        }
-        // eslint-disable-next-line no-param-reassign
-        blob.name = 'croppedImage.jpg';
-        resolve(blob);
-      },
-      extension,
-      1,
-    );
+    canvas.toBlob((blob) => {
+      if (!blob) {
+        reject(new Error('Canvas is empty'));
+        console.error('Canvas is empty');
+        return;
+      }
+      // eslint-disable-next-line no-param-reassign
+      blob.name = 'croppedImage.jpg';
+      resolve(blob);
+    }, extension);
   });
 };
