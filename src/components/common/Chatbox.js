440f7a8df3835e8b65684d0e568f7ea101a7a8c3
spaenleh <basile.1358@gmail.com>
2022-04-11 10:54:47 +0200

    feat: add new chatbox props

--- src/components/common/Chatbox.js
+++ src/components/common/Chatbox.js
@@ -5,17 +5,20 @@ import { MUTATION_KEYS } from '@graasp/query-client';
 import { Map, List } from 'immutable';
 import { Loader } from '@graasp/ui';
 import { hooks, useMutation } from '../../config/queryClient';
-import { HEADER_HEIGHT } from '../../config/constants';
 import { CHATBOX_INPUT_BOX_ID, CHATBOX_ID } from '../../config/selectors';
 import { CurrentUserContext } from '../context/CurrentUserContext';
+import { PERMISSION_LEVELS } from '../../enums';
+import { HEADER_HEIGHT } from '../../config/constants';
 
-const { useItemChat, useMembers, useAvatar } = hooks;
+const { useItemChat, useMembers, useAvatar, useItemMemberships } = hooks;
 
 const Chatbox = ({ item }) => {
   const { data: chat, isLoading: isChatLoading } = useItemChat(item.get('id'));
   const { data: members, isLoading: isMembersLoading } = useMembers([
     ...new Set(chat?.get('messages')?.map(({ creator }) => creator)),
   ]);
+  const { data: itemPermissions, isLoading: isLoadingItemPermissions } =
+    useItemMemberships([item.get('id')]);
   const { data: currentMember, isLoadingCurrentMember } =
     useContext(CurrentUserContext);
   const { mutate: sendMessage } = useMutation(
@@ -29,22 +32,37 @@ const Chatbox = ({ item }) => {
   );
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowHeight(window.innerHeight);
-    };
-    window.addEventListener('resize', handleResize);
+  useEffect(
+    () => {
+      const handleResize = () => {
+        setWindowHeight(window.innerHeight);
+      };
+      window.addEventListener('resize', handleResize);
 
-    // cleanup eventListener
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
+      // cleanup eventListener
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    },
+    // run on first render only
+    [],
+  );
 
-  if (isChatLoading || isLoadingCurrentMember || isMembersLoading) {
+  if (
+    isChatLoading ||
+    isLoadingCurrentMember ||
+    isMembersLoading ||
+    isLoadingItemPermissions
+  ) {
     return <Loader />;
   }
 
+  // only show export chat when user has admin right on the item
+  const isAdmin =
+    itemPermissions
+      .get(0)
+      ?.find((perms) => perms.memberId === currentMember.get('id'))
+      ?.permission === PERMISSION_LEVELS.ADMIN;
   return (
     <GraaspChatbox
       id={CHATBOX_ID}
@@ -54,6 +72,7 @@ const Chatbox = ({ item }) => {
       chatId={item.get('id')}
       messages={List(chat?.get('messages'))}
       height={windowHeight - HEADER_HEIGHT * 2}
+      showAdminTools={isAdmin}
       sendMessageFunction={sendMessage}
       deleteMessageFunction={deleteMessage}
       editMessageFunction={editMessage}
