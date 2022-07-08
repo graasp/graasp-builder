91f64b717416fd28a5809c4d1662d469efc13d70
pyphilia <pyphilia@gmail.com>
2022-07-08 18:12:28 +0200

    refactor: fix tests

--- src/config/selectors.js
+++ src/config/selectors.js
@@ -65,7 +65,6 @@ export const ITEM_FORM_LINK_INPUT_ID = 'itemFormLinkInput';
 export const DASHBOARD_UPLOADER_ID = 'dashboardUploader';
 export const CREATE_ITEM_CLOSE_BUTTON_ID = 'createItemCloseButton';
 export const HEADER_APP_BAR_ID = 'headerAppBar';
-export const HEADER_USER_ID = 'headerUser';
 export const USER_MENU_SIGN_OUT_OPTION_ID = 'userMenuSignOutOption';
 export const NAVIGATION_HIDDEN_PARENTS_ID = 'navigationHiddenParents';
 export const ITEM_LOGIN_SCREEN_ID = 'itemLoginScreen';
