83d882e148e9d9d1466348637672a13c3c5f9af8
Malin Svenberg <malin.svenberg@hotmail.com>
2023-05-26 11:52:11 +0200

    feat: test tour component and adds tour start button

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,7 +1,9 @@
 import Box from '@mui/material/Box';
 
+import { FC, useState } from 'react';
+
 import { BUILDER } from '@graasp/translations';
-import { Loader } from '@graasp/ui';
+import { Button, Loader } from '@graasp/ui';
 
 import { useBuilderTranslation } from '../../config/i18n';
 import { hooks } from '../../config/queryClient';
@@ -14,10 +16,29 @@ import ItemActionsRenderer from './ItemActions';
 import Items from './Items';
 import Main from './Main';
 import NewItemButton from './NewItemButton';
+import { Tour } from './TourContext';
+import steps from './mainTour';
+
+// interface TourInterface {
+//  steps: Step[];
+// }
 
 const Home = (): JSX.Element => {
   const { t: translateBuilder } = useBuilderTranslation();
+  const [run, setRun] = useState(false);
+  // const tourSteps: Step[] = steps;
+  // eslint-disable-next-line
+  console.log(steps);
   const { data: ownItems, isLoading, isError, isSuccess } = hooks.useOwnItems();
+
+  const handleStartTour = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setRun(!run);
+  };
+
+  if (isError) {
+    return <ErrorAlert id={HOME_ERROR_ALERT_ID} />;
+  }
 
   if (isLoading) {
     return <Loader />;
@@ -28,22 +49,34 @@ const Home = (): JSX.Element => {
   }
 
   return (
-    <Main>
-      <UppyContextProvider enable={isSuccess}>
-        <FileUploader />
-        <Box mx={2}>
-          <ItemHeader showNavigation={false} />
-          <Items
-            id={OWNED_ITEMS_ID}
-            defaultSortedColumn={{ updatedAt: 'desc' }}
-            title={translateBuilder(BUILDER.MY_ITEMS_TITLE)}
-            items={ownItems}
-            headerElements={[<NewItemButton key="newButton" />]}
-            ToolbarActions={ItemActionsRenderer}
-          />
-        </Box>
-      </UppyContextProvider>
-    </Main>
+    <Tour run={run}>
+      <Main>
+        <UppyContextProvider enable={isSuccess}>
+          <FileUploader />
+          <Box mx={2}>
+            <Button
+              onClick={handleStartTour}
+              sx={{
+                cursor: 'pointer',
+                flex: 'none',
+                ml: 1,
+              }}
+            >
+              Start Tour
+            </Button>
+            <ItemHeader showNavigation={false} />
+            <Items
+              id={OWNED_ITEMS_ID}
+              defaultSortedColumn={{ updatedAt: 'desc' }}
+              title={translateBuilder(BUILDER.MY_ITEMS_TITLE)}
+              items={ownItems}
+              headerElements={[<NewItemButton key="newButton" />]}
+              ToolbarActions={ItemActionsRenderer}
+            />
+          </Box>
+        </UppyContextProvider>
+      </Main>
+    </Tour>
   );
 };
 
