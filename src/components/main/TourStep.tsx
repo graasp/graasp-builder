b963218e207455f3660f575e5862f92498a92f91
Malin Svenberg <malin.svenberg@hotmail.com>
2023-06-09 10:53:49 +0200

    feat: added more props to tourStep

--- src/components/main/TourStep.tsx
+++ src/components/main/TourStep.tsx
@@ -1,20 +1,36 @@
 import React, { useContext, useEffect } from 'react';
 
-import { Step, TourContext } from './TourContext';
+import { TourContext } from './TourContext';
+import { Step } from './mainTour';
 
 // import steps from './TourSteps.json';
 
 type TourStepProps = {
   step: Step;
   children: React.ReactElement;
+  onTextChange?: (string) => void;
+  focusModal?: boolean;
+  itemId?: string;
+  numberOfItems?: number;
 };
 
-const TourStep: React.FC<TourStepProps> = ({ step, children }) => {
+const TourStep: React.FC<TourStepProps> = ({
+  step,
+  children,
+  onTextChange,
+  focusModal,
+  numberOfItems,
+}) => {
   const { addTourStep } = useContext(TourContext);
 
   useEffect(() => {
-    addTourStep(step); // logic for filtering out the old tour step is in the context
-  }, [step, addTourStep]);
+    let customStep = step;
+    if (onTextChange) customStep = { ...step, onTextChange };
+    if (focusModal) customStep = { ...customStep, focusModal };
+    if (numberOfItems) customStep = { ...customStep, numberOfItems };
+
+    addTourStep(customStep); // logic for filtering out the old tour step is in the context
+  }, [step, addTourStep, onTextChange, focusModal, numberOfItems]);
 
   return children;
 };
