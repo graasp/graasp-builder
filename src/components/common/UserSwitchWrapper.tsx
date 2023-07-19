3c6b7a5558b3c8e788e69399892270469bab6f36
spaenleh <spaenleh@gmail.com>
2023-07-19 09:58:35 +0200

    fix: userswitch props

--- src/components/common/UserSwitchWrapper.tsx
+++ src/components/common/UserSwitchWrapper.tsx
@@ -2,10 +2,10 @@ import { MemberRecord } from '@graasp/sdk/frontend';
 import { BUILDER } from '@graasp/translations';
 import { UserSwitchWrapper as GraaspUserSwitch } from '@graasp/ui';
 
-import { DOMAIN, SIGN_IN_PATH } from '../../config/constants';
+import { SIGN_IN_PATH } from '../../config/constants';
 import { useBuilderTranslation } from '../../config/i18n';
 import { MEMBER_PROFILE_PATH } from '../../config/paths';
-import { hooks, mutations } from '../../config/queryClient';
+import { mutations } from '../../config/queryClient';
 import {
   HEADER_MEMBER_MENU_BUTTON_ID,
   HEADER_MEMBER_MENU_SEE_PROFILE_BUTTON_ID,
@@ -21,11 +21,7 @@ type Props = {
 };
 
 const UserSwitchWrapper = ({ ButtonContent }: Props): JSX.Element => {
-  const {
-    data: member,
-    isLoading,
-    isSuccess: isSuccessUser,
-  } = useCurrentUserContext();
+  const { data: member, isLoading } = useCurrentUserContext();
   const { t: translateBuilder } = useBuilderTranslation();
   const { mutateAsync: signOut } = mutations.useSignOut();
   // todo: does not exist on mutations since we use httpOnly Cookie
@@ -39,7 +35,6 @@ const UserSwitchWrapper = ({ ButtonContent }: Props): JSX.Element => {
       signOut={signOut}
       currentMember={member}
       isCurrentMemberLoading={isLoading}
-      isCurrentMemberSuccess={isSuccessUser}
       // fix in query client
       // switchMember={switchMember as any}
       seeProfileText={translateBuilder(BUILDER.USER_SWITCH_PROFILE_BUTTON)}
@@ -49,9 +44,7 @@ const UserSwitchWrapper = ({ ButtonContent }: Props): JSX.Element => {
       signOutText={translateBuilder(BUILDER.USER_SWITCH_SIGN_OUT_BUTTON)}
       // switchMemberText={translateBuilder(BUILDER.USER_SWITCH_SWITCH_USER_TEXT)}
       profilePath={MEMBER_PROFILE_PATH}
-      domain={DOMAIN}
       redirectPath={SIGN_IN_PATH}
-      useMembers={hooks.useMembers}
       buttonId={HEADER_MEMBER_MENU_BUTTON_ID}
       signInMenuItemId={HEADER_MEMBER_MENU_SIGN_IN_BUTTON_ID}
       signOutMenuItemId={HEADER_MEMBER_MENU_SIGN_OUT_BUTTON_ID}
