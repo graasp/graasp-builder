dd5a8a745a0e26877975d80917c14948fb0ac00d
spaenleh <spaenleh@gmail.com>
2023-07-21 09:28:57 +0200

    fix: add protocol in link without showing it

--- cypress/fixtures/links.ts
+++ cypress/fixtures/links.ts
@@ -10,9 +10,31 @@ export const GRAASP_LINK_ITEM: EmbeddedLinkItemType = {
   description: 'a description for graasp link',
   path: 'ecafbd2a_5688_11eb_ae93_0242ac130002',
   creator: CURRENT_USER,
-  settings: {}, createdAt: new Date(), updatedAt: new Date(),
+  settings: {},
+  createdAt: new Date(),
+  updatedAt: new Date(),
   extra: buildEmbeddedLinkExtra({
     url: 'https://graasp.eu',
+    html: '',
+    thumbnails: ['https://graasp.eu/img/epfl/logo-tile.png'],
+    icons: [
+      'https://graasp.eu/cdn/img/epfl/favicons/favicon-32x32.png?v=yyxJ380oWY',
+    ],
+  }),
+};
+
+export const GRAASP_LINK_ITEM_NO_PROTOCOL: EmbeddedLinkItemType = {
+  id: 'ecafbd2a-5688-11eb-ae91-0242ac130002',
+  type: ItemType.LINK,
+  name: 'graasp link',
+  description: 'a description for graasp link',
+  path: 'ecafbd2a_5688_11eb_ae93_0242ac130002',
+  creator: CURRENT_USER,
+  settings: {},
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  extra: buildEmbeddedLinkExtra({
+    url: 'graasp.eu',
     html: '',
     thumbnails: ['https://graasp.eu/img/epfl/logo-tile.png'],
     icons: [
@@ -35,7 +57,9 @@ export const YOUTUBE_LINK_ITEM: EmbeddedLinkItemType = {
   type: ItemType.LINK,
   name: 'graasp youtube link',
   description: 'a description for graasp youtube link',
-  settings: {}, createdAt: new Date(), updatedAt: new Date(),
+  settings: {},
+  createdAt: new Date(),
+  updatedAt: new Date(),
   creator: CURRENT_USER,
   path: 'gcafbd2a_5688_11eb_ae93_0242ac130002',
   extra: buildEmbeddedLinkExtra({
@@ -51,7 +75,9 @@ export const INVALID_LINK_ITEM: EmbeddedLinkItemType = {
   path: 'gcafbd2a_5688_11eb_ae93_0242ac130001',
   type: ItemType.LINK,
   creator: CURRENT_USER,
-  settings: {}, createdAt: new Date(), updatedAt: new Date(),
+  settings: {},
+  createdAt: new Date(),
+  updatedAt: new Date(),
   name: 'graasp youtube link',
   description: 'a description for graasp youtube link',
   extra: buildEmbeddedLinkExtra({
