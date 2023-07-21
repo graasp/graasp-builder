a38f315ebeff9bd4053f6a19b2afa189309da1df
spaenleh <spaenleh@gmail.com>
2023-07-21 08:55:34 +0200

    fix: adapt url validation to allow omitting the protocol

--- src/utils/item.ts
+++ src/utils/item.ts
@@ -86,14 +86,15 @@ export const isUrlValid = (str?: string): boolean => {
     return false;
   }
   const pattern = new RegExp(
-    '^(https?:\\/\\/)+' + // protocol
+    '^(https?:\\/\\/)?' + // protocol is optional
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '((\\d{1,3}\\.){3}\\d{1,3})|' + // OR ip (v4) address
+      'localhost)' + // OR localhost alias
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
+      '(\\#\\S*)?$', // fragment locator
     'i',
-  ); // fragment locator
+  );
   return pattern.test(str);
 };
 
