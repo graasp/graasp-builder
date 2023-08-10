e7d98d02a912331db38c54378c114bd312dec45e
spaenleh <spaenleh@gmail.com>
2023-08-10 08:15:49 +0200

    fix: delete plugin folder and remove component settings in config

--- cypress.config.ts
+++ cypress.config.ts
@@ -23,10 +23,4 @@ export default defineConfig({
     },
     baseUrl: `http://localhost:${process.env.VITE_PORT || 3333}`,
   },
-  component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
-    },
-  },
 });
