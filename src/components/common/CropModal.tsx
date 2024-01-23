43ce6d8a7782aafb4ef88f1a13b694b8bddead85
spaenleh <spaenleh@gmail.com>
2024-01-23 18:17:50 +0100

    fix: update crop modal (v1)

--- src/components/common/CropModal.tsx
+++ src/components/common/CropModal.tsx
@@ -1,5 +1,10 @@
-import { useRef, useState } from 'react';
-import ReactCrop, { Crop, PixelCrop } from 'react-image-crop';
+import { ReactEventHandler, useRef, useState } from 'react';
+import ReactCrop, {
+  Crop,
+  PixelCrop,
+  centerCrop,
+  makeAspectCrop,
+} from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 
 import Dialog from '@mui/material/Dialog';
@@ -12,11 +17,29 @@ import { Button } from '@graasp/ui';
 
 import { THUMBNAIL_ASPECT } from '../../config/constants';
 import { useBuilderTranslation } from '../../config/i18n';
-import notifier from '../../config/notifier';
 import { CROP_MODAL_CONFIRM_BUTTON_CLASSNAME } from '../../config/selectors';
 import { BUILDER } from '../../langs/constants';
-import { getCroppedImg } from '../../utils/image';
 import CancelButton from './CancelButton';
+
+function centerAspectCrop(
+  mediaWidth: number,
+  mediaHeight: number,
+  aspect: number,
+) {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: '%',
+        width: 90,
+      },
+      aspect,
+      mediaWidth,
+      mediaHeight,
+    ),
+    mediaWidth,
+    mediaHeight,
+  );
+}
 
 export type CropProps = {
   open: boolean;
@@ -31,68 +54,83 @@ const CropModal = ({
   onClose,
   src,
 }: CropProps): JSX.Element => {
-  const [crop, setCrop] = useState<PixelCrop>();
-  const imageRef = useRef<HTMLImageElement>();
+  const [crop, setCrop] = useState<Crop>();
+  const [completedCrop, setCompletedCrop] = useState<PixelCrop>();
+  const imageRef = useRef<HTMLImageElement | null>(null);
   const { t } = useBuilderTranslation();
 
-  const makeClientCrop = async (newCrop: PixelCrop) => {
-    if (imageRef.current && newCrop.width && newCrop.height) {
-      const croppedImage = await getCroppedImg(imageRef.current, newCrop);
-      return croppedImage;
-    }
-    return null;
-  };
-
   const handleOnConfirm = async () => {
-    if (!crop) {
-      notifier({
-        type: 'crop',
-        payload: { error: new Error('crop is undefined') },
-      });
-    } else {
-      const final = await makeClientCrop(crop);
-      onConfirm(final);
+    const image = imageRef.current;
+    if (!image || !completedCrop) {
+      throw new Error('Crop canvas does not exist');
     }
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width,
+      completedCrop.height,
+    );
+    const ctx = offscreen.getContext('2d');
+    if (!ctx) {
+      throw new Error('No 2d context');
+    }
+
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    // devicePixelRatio slightly increases sharpness on retina devices
+    // at the expense of slightly slower render times and needing to
+    // size the image back down if you want to download/upload and be
+    // true to the images natural size.
+    const pixelRatio = window.devicePixelRatio;
+    // const pixelRatio = 1
+
+    offscreen.width = Math.floor(completedCrop.width * scaleX * pixelRatio);
+    offscreen.height = Math.floor(completedCrop.height * scaleY * pixelRatio);
+
+    ctx.scale(pixelRatio, pixelRatio);
+    ctx.imageSmoothingQuality = 'high';
+
+    const cropX = completedCrop.x * scaleX;
+    const cropY = completedCrop.y * scaleY;
+
+    const centerX = image.naturalWidth / 2;
+    const centerY = image.naturalHeight / 2;
+
+    ctx.save();
+
+    // 5) Move the crop origin to the canvas origin (0,0)
+    ctx.translate(-cropX, -cropY);
+    // 4) Move the origin to the center of the original position
+    ctx.translate(centerX, centerY);
+    // 1) Move the center of the image to the origin (0,0)
+    ctx.translate(-centerX, -centerY);
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      image.naturalWidth,
+      image.naturalHeight,
+      0,
+      0,
+      image.naturalWidth,
+      image.naturalHeight,
+    );
+
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: 'image/png',
+    });
+    onConfirm(blob);
   };
 
   // If you setState the crop in here you should return false.
-  const onImageLoaded = (img: HTMLImageElement) => {
+  const onImageLoaded: ReactEventHandler<HTMLImageElement> = (event) => {
     if (!imageRef.current) {
-      imageRef.current = img;
-    }
-    const { width: imgWidth, height: imgHeight } = img;
-
-    const aspect = THUMBNAIL_ASPECT;
-    const outputImageAspectRatio = aspect;
-    const inputImageAspectRatio = imgWidth / imgHeight;
-
-    let width = imgWidth;
-    let height = imgHeight;
-    // if it's bigger than our target aspect ratio
-    if (inputImageAspectRatio > outputImageAspectRatio) {
-      width = imgHeight * outputImageAspectRatio;
-    } else if (inputImageAspectRatio < outputImageAspectRatio) {
-      height = imgWidth / outputImageAspectRatio;
+      imageRef.current = event.currentTarget;
     }
 
-    const y = Math.floor((imgHeight - height) / 2);
-    const x = Math.floor((imgWidth - width) / 2);
-
-    const newCrop = {
-      width,
-      height,
-      x,
-      y,
-      aspect,
-      unit: 'px',
-    } as PixelCrop;
-    setCrop(newCrop);
-
-    return false; // Return false if you set crop state in here.
-  };
-
-  const onCropChange = (newCrop: Crop, _percentageCrop: Crop) => {
-    setCrop(newCrop as PixelCrop);
+    const { width: imgWidth, height: imgHeight } = event.currentTarget;
+    setCrop(centerAspectCrop(imgWidth, imgHeight, THUMBNAIL_ASPECT));
   };
 
   const label = 'crop-modal-title';
@@ -105,12 +143,22 @@ const CropModal = ({
           {t(BUILDER.CROP_IMAGE_MODAL_CONTENT_TEXT)}
         </DialogContentText>
         <ReactCrop
-          src={src}
-          crop={crop ?? {}}
+          crop={crop}
+          onChange={(_, percentageCrop) => setCrop(percentageCrop)}
+          onComplete={(c) => setCompletedCrop(c)}
+          aspect={THUMBNAIL_ASPECT}
+          // circularCrop
           ruleOfThirds
-          onImageLoaded={onImageLoaded}
-          onChange={onCropChange}
-        />
+        >
+          <img
+            ref={imageRef}
+            alt="Crop me"
+            width="100%"
+            height="100%"
+            src={src}
+            onLoad={onImageLoaded}
+          />
+        </ReactCrop>
       </DialogContent>
       <DialogActions>
         <CancelButton onClick={onClose} />
