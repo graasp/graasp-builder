07692d61d2062e02da1fd1c29a9816dc10209a50
spaenleh <spaenleh@gmail.com>
2024-03-11 10:15:38 +0100

    fix: update test descriptions

--- cypress/e2e/item/upload/dropzoneUpload.cy.ts
+++ cypress/e2e/item/upload/dropzoneUpload.cy.ts
@@ -3,27 +3,29 @@ import { DROPZONE_HELPER_ID } from '@/config/selectors';
 
 import { SAMPLE_ITEMS, SAMPLE_PUBLIC_ITEMS } from '../../../fixtures/items';
 
-describe('home screen - dropzone helper visibility', () => {
-  beforeEach(() => {
-    cy.setUpApi();
+describe('Dropzone Helper Visibility', () => {
+  describe('Home screen', () => {
+    beforeEach(() => {
+      cy.setUpApi();
+    });
+
+    it(' should display the dropzone on the home screen when no items', () => {
+      cy.visit('/');
+      cy.get(`#${DROPZONE_HELPER_ID}`).should('be.visible');
+    });
   });
 
-  it('should display dropzone helper on the home screen when no items', () => {
-    cy.visit('/');
-    cy.get(`#${DROPZONE_HELPER_ID}`).should('be.visible');
-  });
-});
+  describe('Empty folder', () => {
+    it('should show dropzone helper when no items (logged in)', () => {
+      cy.setUpApi(SAMPLE_ITEMS);
+      cy.visit(buildItemPath(SAMPLE_ITEMS.items[1].id));
+      cy.get(`#${DROPZONE_HELPER_ID}`).should('be.visible');
+    });
 
-describe('empty folder - dropzone helper visibility', () => {
-  it('user logs in - show dropzone helper when no items', () => {
-    cy.setUpApi(SAMPLE_ITEMS);
-    cy.visit(buildItemPath(SAMPLE_ITEMS.items[1].id));
-    cy.get(`#${DROPZONE_HELPER_ID}`).should('be.visible');
-  });
-
-  it('user logs out - hide dropzone helper', () => {
-    cy.setUpApi({ ...SAMPLE_PUBLIC_ITEMS, currentMember: null });
-    cy.visit(buildItemPath(SAMPLE_PUBLIC_ITEMS.items[2].id));
-    cy.get(`#${DROPZONE_HELPER_ID}`).should('not.exist');
+    it('should hide dropzone helper when no items (logged out)', () => {
+      cy.setUpApi({ ...SAMPLE_PUBLIC_ITEMS, currentMember: null });
+      cy.visit(buildItemPath(SAMPLE_PUBLIC_ITEMS.items[2].id));
+      cy.get(`#${DROPZONE_HELPER_ID}`).should('not.exist');
+    });
   });
 });
