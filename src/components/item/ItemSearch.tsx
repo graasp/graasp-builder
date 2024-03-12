f29d1765d6d24362f917f613f0ceedc0e0d80828
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-03-12 18:01:09 +0100

    fix: add item search in folder view (#1080)
    * fix: add item search in folder view
    
    * refactor: update ui

--- src/components/item/ItemSearch.tsx
+++ src/components/item/ItemSearch.tsx
@@ -31,7 +31,7 @@ export const useItemSearch = ({
   onSearch,
 }: {
   onSearch?: () => void;
-}): {
+} = {}): {
   results?: DiscriminatedItem[];
   text: string;
   input: JSX.Element;
@@ -52,6 +52,7 @@ export const useItemSearch = ({
       value={searchText}
       inputBaseId={ITEM_SEARCH_INPUT_ID}
       placeholder={translateBuilder(BUILDER.ITEM_SEARCH_PLACEHOLDER)}
+      size="small"
     />
   );
   return { text: searchText, input: itemSearchInput };
