efcda2e98293eb3d3d36d7878c12e24a3403e7d5
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-03-26 14:12:23 +0100

    feat: add geolocation modal (#1112)
    * refactor: add geolocation modal
    
    * refactor: add translations
    
    * refactor: update fr trans
    
    * refactor: upgrade query client

--- src/components/item/settings/GeolocationPicker.tsx
+++ src/components/item/settings/GeolocationPicker.tsx
@@ -11,6 +11,8 @@ import { useBuilderTranslation } from '@/config/i18n';
 import { hooks, mutations } from '@/config/queryClient';
 import { BUILDER } from '@/langs/constants';
 
+import GeolocationModalButton from './GeolocationModalButton';
+
 const GeolocationPicker = ({
   item,
 }: {
@@ -48,37 +50,40 @@ const GeolocationPicker = ({
   const isDisabled = Boolean(geoloc && geoloc?.item?.id !== item.id);
 
   return (
-    <Stack mt={2}>
-      <Stack>
-        <Typography variant="h6">
-          {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_TITLE)}
-        </Typography>
-        <Typography variant="subtitle1">
-          {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_EXPLANATION)}
-        </Typography>
-      </Stack>
-      <Stack direction="row" alignItems="center">
-        <Stack flexGrow={1}>
-          <MapGeolocationPicker
-            onChangeOption={onChangeOption}
-            initialValue={geoloc?.addressLabel ?? undefined}
-            useSuggestionsForAddress={hooks.useSuggestionsForAddress}
-          />
-        </Stack>
+    <>
+      <Stack mt={2} gap={2}>
         <Stack>
-          <Tooltip title={t(BUILDER.ITEM_SETTINGS_CLEAR_GEOLOCATION)}>
-            <IconButton onClick={clearGeoloc}>
-              <Clear />
-            </IconButton>
-          </Tooltip>
+          <Typography variant="h6">
+            {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_TITLE)}
+          </Typography>
+          <Typography variant="subtitle1">
+            {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_EXPLANATION)}
+          </Typography>
         </Stack>
+        <Stack direction="row" alignItems="center">
+          <Stack flexGrow={1}>
+            <MapGeolocationPicker
+              onChangeOption={onChangeOption}
+              initialValue={geoloc?.addressLabel ?? undefined}
+              useSuggestionsForAddress={hooks.useSuggestionsForAddress}
+            />
+          </Stack>
+          <Stack>
+            <Tooltip title={t(BUILDER.ITEM_SETTINGS_CLEAR_GEOLOCATION)}>
+              <IconButton onClick={clearGeoloc}>
+                <Clear />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+        </Stack>
+        {isDisabled && (
+          <Typography variant="caption">
+            {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_INHERITED_EXPLANATION)}
+          </Typography>
+        )}
       </Stack>
-      {isDisabled && (
-        <Typography variant="caption">
-          {t(BUILDER.ITEM_SETTINGS_GEOLOCATION_INHERITED_EXPLANATION)}
-        </Typography>
-      )}
-    </Stack>
+      <GeolocationModalButton item={item} />
+    </>
   );
 };
 
