46767d098fa1a4a462204f38990607ff7b37fb53
spaenleh <spaenleh@gmail.com>
2024-05-24 16:40:20 +0200

    fix: update ui and remove document displayname test

--- cypress/e2e/item/create/createDocument.cy.ts
+++ cypress/e2e/item/create/createDocument.cy.ts
@@ -4,13 +4,7 @@ import {
   PackedFolderItemFactory,
 } from '@graasp/sdk';
 
-import {
-  CREATE_ITEM_BUTTON_ID,
-  CREATE_ITEM_DOCUMENT_ID,
-  ITEM_FORM_CONFIRM_BUTTON_ID,
-  ITEM_FORM_DISPLAY_NAME_INPUT_ID,
-  ITEM_FORM_NAME_INPUT_ID,
-} from '@/config/selectors';
+import { ITEM_FORM_CONFIRM_BUTTON_ID } from '@/config/selectors';
 
 import { HOME_PATH, buildItemPath } from '../../../../src/config/paths';
 import ItemLayoutMode from '../../../../src/enums/itemLayoutMode';
@@ -72,21 +66,6 @@ describe('Create Document', () => {
       'have.prop',
       'disabled',
       true,
-    );
-  });
-
-  it('autoComplete document displayName', () => {
-    cy.setUpApi();
-    cy.visit(HOME_PATH);
-
-    cy.switchMode(ItemLayoutMode.List);
-
-    cy.get(`#${CREATE_ITEM_BUTTON_ID}`).click();
-    cy.get(`#${CREATE_ITEM_DOCUMENT_ID}`).click();
-    cy.get(`#${ITEM_FORM_NAME_INPUT_ID}`).type('Test Name');
-    cy.get(`#${ITEM_FORM_DISPLAY_NAME_INPUT_ID}`).should(
-      'have.value',
-      'Test Name',
     );
   });
 });
