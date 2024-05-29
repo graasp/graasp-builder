445fb04da240998270c14abab958183840ff8a11
spaenleh <spaenleh@gmail.com>
2024-05-29 10:34:52 +0200

    fix: embedding setting and iframe display

--- src/components/item/form/link/LinkForm.tsx
+++ src/components/item/form/link/LinkForm.tsx
@@ -244,25 +244,26 @@ const LinkForm = ({
                 // only show this options when embedding is allowed and there is no html code
                 // as the html will take precedence over showing the site as an iframe
                 // and some sites like daily motion actually allow both, we want to allow show the html setting
-                linkData?.isEmbeddingAllowed && linkData?.html === '' && (
-                  <StyledFormControlLabel
-                    value={LinkType.Embedded}
-                    label={embeddedLinkPreview}
-                    control={<Radio />}
-                    slotProps={{
-                      typography: {
-                        width: '100%',
-                        minWidth: '0px',
-                      },
-                    }}
-                    sx={{
-                      // this ensure the iframe takes up all horizontal space
-                      '& iframe': {
-                        width: '100%',
-                      },
-                    }}
-                  />
-                )
+                linkData?.isEmbeddingAllowed &&
+                  (linkData?.html === undefined || linkData?.html === '') && (
+                    <StyledFormControlLabel
+                      value={LinkType.Embedded}
+                      label={embeddedLinkPreview}
+                      control={<Radio />}
+                      slotProps={{
+                        typography: {
+                          width: '100%',
+                          minWidth: '0px',
+                        },
+                      }}
+                      sx={{
+                        // this ensure the iframe takes up all horizontal space
+                        '& iframe': {
+                          width: '100%',
+                        },
+                      }}
+                    />
+                  )
               }
             </RadioGroup>
           ) : (
