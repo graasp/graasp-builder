d468e052a64f09423928ca737fc3f373fff1f503
spaenleh <spaenleh@gmail.com>
2024-07-04 15:27:40 +0200

    fix: warning about `UNSAFE_componentWillMount` using react-helmet-async

--- src/components/item/ItemMain.tsx
+++ src/components/item/ItemMain.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 import { Box, Divider, Typography, styled } from '@mui/material';
 
--- src/components/pages/HomeScreen.tsx
+++ src/components/pages/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 import { Box, LinearProgress } from '@mui/material';
 
